c7918282f114dbe3dcacacd8b2074d25f8aec4aa
Nabin Kumar Soni <nabinkumar570@gmail.com>
2025-01-22 13:59:47 +0530

    created new project to learn basic

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 
 import App from './App.jsx'
 
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//     href: "https://google.com",
+//     target: '_blank'
+//   }, 
+//   children: 'Click here to visit Google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Click here to visit Google</a>
+)
+
+const reactElement = React.createElement('a', 
+  {
+    href: "https://google.com",
+    target: '_blank'
+  }, 
+  'Click here to visit Google'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    // reactElement
+  <App />
 )
