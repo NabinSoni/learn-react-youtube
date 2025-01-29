74a34be6037e5fa087a87bde34b0331c59b9d24c
Nabin Kumar Soni <nabinkumar570@gmail.com>
2025-01-29 10:35:15 +0530

    added tailwind css project

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,30 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [counter, setCounter] = useState(15)
+
+  const addCount = () => {
+    if (counter < 5) 
+      setCounter(counter + 1)
+  }
+
+  const reduceCount = () => {
+    if(counter != 0) 
+      setCounter(counter - 1)
+  }
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Chai aur React | {counter}</h1>
+     <button onClick={addCount}>
+      Add Count {counter}</button>
+     <br />
+     <button onClick={reduceCount}>
+      Reduce Count {counter}</button>
     </>
   )
 }
